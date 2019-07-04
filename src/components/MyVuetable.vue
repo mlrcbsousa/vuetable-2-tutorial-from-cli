0f49863545f98d63554bea1503acd582f737d117
Manuel Sousa <mlrcbsousa@gmail.com>
2019-07-04 20:15:35 +0200

    lesson-11

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -1,40 +1,159 @@
 // MyVuetable.vue
 
 <template>
-  <vuetable ref="vuetable"
-    api-url="https://vuetable.ratiw.net/api/users"
-    :fields="fields"
-  ></vuetable>
+  <div class="ui container">
+    <div class="vuetable-pagination ui basic segment grid">
+      <vuetable-pagination-info ref="paginationInfoTop"></vuetable-pagination-info>
+
+      <vuetable-pagination ref="paginationTop"
+        @vuetable-pagination:change-page="onChangePage"
+      ></vuetable-pagination>
+    </div>
+
+    <vuetable ref="vuetable"
+      api-url="https://vuetable.ratiw.net/api/users"
+      pagination-path=""
+      multi-sort-key="ctrl"
+      detail-row-component="my-detail-row"
+      :appendParams="moreParams"
+      :multi-sort="true"
+      :fields="fields"
+      :per-page="20"
+      :sort-order="sortOrder"
+      @vuetable:pagination-data="onPaginationData"
+      @vuetable:cell-clicked="onCellClicked"
+    >
+
+      <template slot="actions" scope="props">
+        <div class="custom-actions">
+          <button
+            class="ui basic button"
+            @click="onAction('view-item', props.rowData, props.rowIndex)"
+          >
+            <i class="zoom icon"></i>
+          </button>
+          <button
+            class="ui basic button"
+            @click="onAction('edit-item', props.rowData, props.rowIndex)"
+          >
+            <i class="edit icon"></i>
+          </button>
+          <button
+            class="ui basic button"
+            @click="onAction('delete-item', props.rowData, props.rowIndex)"
+          >
+            <i class="delete icon"></i>
+          </button>
+        </div>
+      </template>
+
+    </vuetable>
+      <!-- :css="css" -->
+
+    <div class="vuetable-pagination ui basic segment grid">
+      <vuetable-pagination-info ref="paginationInfo"></vuetable-pagination-info>
+
+      <vuetable-pagination ref="pagination"
+        @vuetable-pagination:change-page="onChangePage"
+      ></vuetable-pagination>
+    </div>
+  </div>
 </template>
 
 <script>
 import Vuetable from 'vuetable-2/src/components/Vuetable';
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
+import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo';
+import accounting from 'accounting';
+import moment from 'moment';
+import Vue from 'vue';
+import CustomActions from './CustomActions';
+
+Vue.component('custom-actions', CustomActions);
 
 export default {
   components: {
     Vuetable,
+    VuetablePagination,
+    VuetablePaginationInfo,
   },
   data() {
     return {
-      fields: [
-        'name', 'email',
+      // css: {
+      //   ascendingIcon: 'glyphicon glyphicon-chevron-up',
+      //   descendingIcon: 'glyphicon glyphicon-chevron-down',
+      // },
+      sortOrder: [
         {
-          name: 'birthdate',
+          field: 'email',
+          sortField: 'email',
+          direction: 'asc',
+        },
+      ],
+      fields: [
+        {
+          name: '__sequence',
+          title: '#',
+          titleClass: 'center aligned',
+          dataClass: 'right aligned',
+        },
+        {
+          name: '__handle',
+          dataClass: 'center aligned',
+        },
+        {
+          name: '__checkbox',
           titleClass: 'center aligned',
           dataClass: 'center aligned',
         },
         {
+          name: 'name',
+          sortField: 'name',
+        },
+        {
+          name: 'email',
+          sortField: 'email',
+        },
+        {
+          name: 'age',
+          sortField: 'birthdate',
+          dataClass: 'center aligned',
+        },
+        {
+          name: 'birthdate',
+          sortField: 'birthdate',
+          titleClass: 'center aligned',
+          dataClass: 'center aligned',
+          callback: 'formatDate|DD-MM-YYYY',
+        },
+        {
           name: 'nickname',
+          sortField: 'nickname',
           callback: 'allcap',
         },
         {
           name: 'gender',
+          sortField: 'gender',
           titleClass: 'center aligned',
           dataClass: 'center aligned',
           callback: 'genderLabel',
         },
         {
           name: 'salary',
+          sortField: 'salary',
+          titleClass: 'center aligned',
+          dataClass: 'right aligned',
+          callback: 'formatNumber',
+        },
+        {
+          name: '__component:custom-actions',
+          title: 'Actions',
+          titleClass: 'center aligned',
+          dataClass: 'center aligned',
+        },
+        {
+          name: '__slot:actions',
+          title: 'Actions',
           titleClass: 'center aligned',
           dataClass: 'center aligned',
         },
@@ -49,6 +168,24 @@ export default {
       return value === 'M'
         ? '<span class="ui teal label"><i class="large man icon"></i>Male</span>'
         : '<span class="ui pink label"><i class="large woman icon"></i>Female</span>';
+    },
+    formatNumber(value) {
+      return accounting.formatNumber(value, 2);
+    },
+    formatDate(value, fmt = 'DD MM YYYY') {
+      return (value == null)
+        ? ''
+        : moment(value, 'YYYY-MM-DD').format(fmt);
+    },
+    onPaginationData(paginationData) {
+      this.$refs.paginationTop.setPaginationData(paginationData);
+      this.$refs.paginationInfoTop.setPaginationData(paginationData);
+
+      this.$refs.pagination.setPaginationData(paginationData);
+      this.$refs.paginationInfo.setPaginationData(paginationData);
+    },
+    onChangePage(page) {
+      this.$refs.vuetable.changePage(page);
     },
   },
 };
